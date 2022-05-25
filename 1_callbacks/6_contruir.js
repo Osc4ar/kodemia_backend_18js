8d05893a93f943e4ecb693d2bf1bcf353abebac2
Osc4ar <oscar_97.10@outlook.com>
2022-05-24 21:45:13 -0500

    Ejemplo de Callback Hell

--- 1_callbacks/6_contruir.js
+++ 1_callbacks/6_contruir.js
@@ -1,26 +1,29 @@
 // Estamos construyendo un muro, y hay tres acciones que deben hacerse una después de la otra
-function construir(muro) {
+function construir(muro, callback) {
   console.log("Construyendo...");
 
   setTimeout(() => {
     muro.contruido = true;
+    callback(muro); // El muro ya fue construido
   }, 500);
 }
 
-function aplanar(muro) {
+function aplanar(muro, callback) {
   console.log("Aplanando...");
 
   setTimeout(() => {
     muro.aplanado = true;
-  }, 200);
+    callback(muro);
+  }, 300);
 }
 
-function pintar(muro) {
+function pintar(muro, callback) {
   console.log("Pintando...");
 
   setTimeout(() => {
     muro.pintado = true;
-  }, 100);
+    callback(muro);
+  }, 240);
 }
 
 const muro = {
@@ -29,15 +32,19 @@ const muro = {
   pintado: false,
 };
 
-const muroContruido = construir(muro);
-console.log('Muro Construido:', muroContruido)
+// const muroContruido = construir(muro);
+// console.log("Muro Construido:", muroContruido);
 
-const muroAplanado = aplanar(muroContruido);
-console.log('Muro Aplanado:', muroAplanado)
+// const muroAplanado = aplanar(muroContruido);
+// console.log("Muro Aplanado:", muroAplanado);
 
-const muroPintado = pintar(muroAplanado);
-console.log('Muro Pintado:', muroPintado)
+// const muroPintado = pintar(muroAplanado);
+// console.log("Muro Pintado:", muroPintado);
 
-
-
-
+construir(muro, (muroConstruido) => {
+    aplanar(muroConstruido, (muroAplanado) => {
+        pintar(muroAplanado, (muroPintado) => {
+            console.log('Muro terminado!', muroPintado)
+        })
+    })
+})
