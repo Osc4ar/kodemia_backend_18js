d6704b6989c6557b33deb8754817104ec93537ee
Osc4ar <oscar_97.10@outlook.com>
2022-05-27 20:32:26 -0500

    Getting request details

--- 3_http/1_hola_mundo.js
+++ 3_http/1_hola_mundo.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 // Creamos un servidor usando createServer
 const server = http.createServer((request, response) => {
+  // Consultamos la URL de la request
+  console.log("URL:", request.url);
+  // Consultamos el método de la request
+  console.log("Método:", request.method);
+
   // Vamos a responder al cliente
   response.write("Hola desde mi server!"); // escribimos la respuesta
   response.end(); // terminamos la respuesta y la enviamos
