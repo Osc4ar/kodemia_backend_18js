010b9522f62d1395d70fd111b3d6d3220bf3f49b
Osc4ar <oscar_97.10@outlook.com>
2022-05-31 20:56:32 -0500

    JSON responses basic example

--- 4_express/index.js
+++ 4_express/index.js
@@ -6,6 +6,41 @@ server.get("/", (request, response) => {
   response.send("Hola Koders!");
 });
 
+server.post("/users", (req, res) => {
+  const respuesta = {
+    mensaje: "Aqui puedes crear usuarios",
+  };
+
+  // Va a convertir el objeto a JSON
+  // lo va a mandar como respuesta
+  // y va a crear el header Content-Type
+  res.json(respuesta);
+});
+
+server.get("/koder", (req, res) => {
+  const respuesta = {
+    mensaje: "Aqui estan todos los koders",
+  };
+
+  res.json(respuesta);
+})
+
+server.post("/koder", (req, res) => {
+  const respuesta = {
+    mensaje: "Aqui puedes crear koders",
+  };
+
+  res.json(respuesta);
+})
+
+server.put("/koder", (req, res) => {
+  const respuesta = {
+    mensaje: "Aqui puedes sustituir un koder",
+  };
+
+  res.json(respuesta);
+})
+
 server.listen(8000, () => {
   console.log("Servidor ejecutandose");
 });
