f7bd59fded56f4c13d1c4b9b1e111c7a8d2d8248
Osc4ar <oscar_97.10@outlook.com>
2022-05-31 21:58:08 -0500

    Example of post using JSON and files

--- 4_express/index.js
+++ 4_express/index.js
@@ -1,20 +1,44 @@
 const express = require("express");
+const fs = require("fs/promises");
 
 const server = express();
+
+// middleware para convertir request a JSON
+server.use(express.json())
 
 server.get("/", (request, response) => {
   response.send("Hola Koders!");
 });
 
-server.post("/users", (req, res) => {
-  const respuesta = {
-    mensaje: "Aqui puedes crear usuarios",
-  };
+server.get("/koders", async (req, res) => {
+  const archivo = await fs.readFile("koders.json", "utf8"); // el archivo es un String
+  console.log(archivo);
+  const objeto = JSON.parse(archivo); // convierte un string a un objeto
+  console.log(objeto);
+  const koders = objeto.koders; // accedemos solo a los koders que estan en un arreglo
 
-  // Va a convertir el objeto a JSON
-  // lo va a mandar como respuesta
-  // y va a crear el header Content-Type
-  res.json(respuesta);
+  res.json(koders)
+});
+
+server.post("/koders", async (req, res) => {
+  console.log('body:', req.body);
+  const koder = req.body;
+
+  // Cargar Koders
+  const archivo = await fs.readFile("koders.json", "utf8"); // el archivo es un String
+  const objeto = JSON.parse(archivo); // convierte un string a un objeto
+  const koders = objeto.koders; // accedemos solo a los koders que estan en un arreglo
+
+  // Agregar un nuevo Koder
+  koders.push(koder)
+  
+  // Guardar cambios
+  const nuevoArchivo = JSON.stringify(objeto, null, 2) // Convertimos el objeto a un String nuevo
+  await fs.writeFile("koders.json", nuevoArchivo, "utf8");
+
+  // Enviamos respuesta
+  res.status(201) // Estado de creado
+  res.json(koders)
 });
 
 server.get("/koder", (req, res) => {
